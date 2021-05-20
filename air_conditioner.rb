be80d0adefa4174ca2b4155a6446a7a61279f10a
sakasegawajunki <j.sakasegawa18@gmail.com>
2021-05-20 12:42:37 +0000

    [Finish]6

--- air_conditioner.rb
+++ air_conditioner.rb
@@ -2,6 +2,7 @@ input_lines = readlines
 
 room = input_lines[0].to_i
 air_conditioner = input_lines[1].to_i
+air_strong = input_lines[2].to_i
 
 temperature_diff = (room - air_conditioner).abs
 
@@ -16,4 +17,21 @@ elsif  temperature_diff > 10 then
   require_time = 60
 end
 
+case air_strong
+ when 1 then
+  require_time
+  
+ when 2 then
+  require_time = require_time-5
+  
+ when 3 then
+  
+  require_time = require_time-10
+  
+ else
+   p"風量の値は1～3にして下さい"
+  exit
+
+end
+  
 p require_time
